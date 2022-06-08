9600d66c1475e195a6f044d56fc77e3fcac19aad
Baruch <baruch.bazak.dev@gmail.com>
2022-06-08 19:05:30 +0300

    test1

--- app.py
+++ app.py
@@ -19,6 +19,47 @@ def get_db(db_name):
     return db
 
 
+@app.route('/first-init')
+def ping_server():
+    client = MongoClient(host=os.environ['MONGO_URI'],
+                         port=27017,
+                         username=os.environ['MONGO_USERNAME'],
+                         password=os.environ['MONGO_PASS'],
+                         authSource="admin")
+    db_names = client.list_database_names()
+    if 'employees' in db_names:
+        return "first init run.."
+    else:
+        db = get_db("employees")
+        db.devops.insertMany([
+            {
+                "id": 1,
+                "f_name": "Baruch",
+                "l_name": "Bazak",
+                "city": "Tel-Mond",
+                "address": "azofit",
+                "phone_number": "054-399-7477"
+            },
+            {
+                "id": 2,
+                "f_name": "Tom",
+                "l_name": "Brovender",
+                "city": "Herzelya",
+                "address": "DC",
+                "phone_number": "054-459-7477"
+            },
+            {
+                "id": 3,
+                "f_name": "Amit",
+                "l_name": "Zarmon",
+                "city": "Mevaseret",
+                "address": "aahaghgd",
+                "phone_number": "054-319-7477"
+            }
+        ])
+        return f"Welcome to the world of DevOps."
+
+
 @app.route('/')
 def ping_server():
     return f"Welcome to the world of DevOps."
